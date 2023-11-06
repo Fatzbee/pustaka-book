89de958ec44ecae1e28d74a6338d872385f6244b
fatzbee <alfathbisa@gmail.com>
2023-11-06 21:30:49 +0700

    C-i 80% Progress

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>latihan1</title>
 <html>
-    <head>
-        <title>Latihan 1</title>
-    </head>
-    <body>
-        
-        halo kawan.. Yuk kita belajar web programming..!!<br>
-        Nilai 1 = <?= $nilai1 ?>
-        Nilai 2 = <?= $nilai2 ?>
-        ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 ." + ". $nilai2. " = ". $hasil; ?>
-
-    </body>
-
+  <head>
+    <title>latihan 1</title>
+  </head>
+  <body>
+    Haloo kawan.. Yuk kita belajar Web Programming..!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 ." + ". $nilai2 ." = ". $hasil;  ?>
+  </body>
 </html>
